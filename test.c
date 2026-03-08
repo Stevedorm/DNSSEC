affccb09d5988c3cb7b29a31ed07e21c931d09a7
stdormady <dormadsa@dukes.jmu.edu>
2026-03-08 16:05:52 -0400

    further testing and TO-DO

--- test.c
+++ test.c
@@ -3,6 +3,15 @@
 FILE *pcap = NULL;
 
 int main (int argc, char *argv[]) {
-    printf("Hello, World!\n");
+
+    FILE *pcap = fopen("captures/delv_valid.pcap", "rb");
+    if (pcap == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+    printf("File opened successfully\n");
+    
+    fclose(pcap);
+
     return 0;
 }
